2ccb0f38d87d94dff462c7e1db30a3f918250909
MfNiimoto <162495567+MfNiimoto@users.noreply.github.com>
2025-01-27 14:02:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from PyQt6 import QtWidgets
 import sqlite3
 import sys
 
-# Importa os módulos gerados a partir dos arquivos .ui
-from login import Ui_MainWindow as UiLogin
+from login import Ui_Form as UiLogin
 from paineladministrador import Ui_MainWindow as UiAdminPanel
 from mainwindow import Ui_MainWindow as UiMainWindow
 
@@ -13,14 +12,12 @@ class LoginScreen(QtWidgets.QMainWindow):
         self.ui = UiLogin()
         self.ui.setupUi(self)
 
-        # Conecta o botão de login à função de verificação
         self.ui.loginButton.clicked.connect(self.handle_login)
 
     def handle_login(self):
         username = self.ui.usernameField.text()
         password = self.ui.passwordField.text()
 
-        # Verificação no banco de dados
         with sqlite3.connect('dados.db') as conn:
             cursor = conn.cursor()
             cursor.execute("SELECT role FROM user WHERE username = ? AND password = ?", (username, password))
@@ -50,18 +47,14 @@ class MainWindow(QtWidgets.QMainWindow):
 class MainApp(QtWidgets.QStackedWidget):
     def __init__(self):
         super().__init__()
-
-        # Instancia as telas
         self.login_screen = LoginScreen()
         self.admin_panel = AdminPanel()
         self.main_window = MainWindow()
 
-        # Adiciona as telas ao QStackedWidget
         self.addWidget(self.login_screen)
         self.addWidget(self.admin_panel)
         self.addWidget(self.main_window)
 
-        # Define a tela inicial como login
         self.setCurrentWidget(self.login_screen)
 
     def show_admin_panel(self):
